3288d12eb52e15dadb58e8928f7ce620e0134ca5
fanghao0506 <fanghao0506@163.com>
2023-06-29 17:19:28 +0800

    1

---
+++ poem/js/navigation.js
@@ -0,0 +1,13 @@
+document.write(`
+  <div id="navigation">
+    <span class="nav-tab"><a href="poem1.html">夏夜</a></span>
+    <span class="nav-tab"><a href="poem2.html">西北</a></span>
+    <span class="nav-tab"><a href="poem3.html">归去来兮</a></span>
+    <span class="nav-tab"><a href="poem4.html">宇宙和我</a></span>
+    <span class="nav-tab"><a href="poem5.html">坠落</a></span>
+    <span class="nav-tab"><a href="poem6.html">罗马</a></span>
+    <span class="nav-tab"><a href="poem7.html">思念</a></span>
+    <span class="nav-tab"><a href="poem8.html">长安忆</a></span>
+    <span class="nav-tab"><a href="index.html">关于我</a></span>
+  </div>
+`);
